d83053598e0be3403545784b52a68feb4b5f7f78
Pietra Sophia <133168161+pietrao0@users.noreply.github.com>
2023-08-10 23:28:41 +0000

    Tentativa de criação do endpoint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { selectUsuarios, selectUsuario } from "./bd.js";
 import dotenv from "dotenv";
 import express from "express"; 
 import { selectUsuarios } from "./bd.js";
+import { selectUsuarios, selectUsuario, insertUsuario } from "./bd.js";
 
 dotenv.config();     // Requisição do pacote do express
 const app = express();              // Instancia o Express
@@ -36,6 +36,18 @@ app.get("/usuario/:id", async (req, res) => {
     const usuario = await selectUsuario(req.params.id);
     if (usuario.length > 0) res.json(usuario);
     else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+app.use(express.json());
+
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
