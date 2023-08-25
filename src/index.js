9531038ae62814d6370b1bbac6f2277c9e070e14
Pietra Sophia <133168161+pietrao0@users.noreply.github.com>
2023-08-25 17:52:19 +0000

    Terminei pae

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();              
 const port = 3000;                  
 app.use(express.json());
- .app.use(roteadorUsuario);
+app.use(roteadorUsuario);
 app.use(roteadorLogin);
 app.use(express.urlencoded({ extended: true }));
 
